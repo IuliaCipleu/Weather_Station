c291701d57e23092ee75622241732845e864c3d1
Iulia Cipleu <115873337+IuliaCipleu@users.noreply.github.com>
2023-12-02 21:54:30 +0200

    Add files via upload

--- tempHumSerial.ino
+++ tempHumSerial.ino
@@ -1,17 +1,26 @@
 #include "DHT.h"
 
-#define DHTPIN 7     // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT22 // Type of DHT sensor (DHT22)
+#define DHTPIN 7       // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22  // Type of DHT sensor (DHT22)
+#define BLUE 10
+#define GREEN 11
+#define RED 12
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  pinMode(BLUE, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+  digitalWrite(BLUE, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(RED, LOW);
   Serial.begin(9600);
   dht.begin();
 }
 
 void loop() {
-  delay(2000); // Wait for 2 seconds between readings
+  delay(2000);  // Wait for 2 seconds between readings
 
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
@@ -26,4 +35,20 @@ void loop() {
   Serial.print(humidity);
   Serial.print("%  Temperature: ");
   Serial.println(temperature);
+
+  if (temperature < 19.0) {
+    digitalWrite(BLUE, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, LOW);
+  }
+  if (temperature >= 19.0 && temperature < 22.0) {
+    digitalWrite(BLUE, LOW);
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(RED, LOW);
+  }
+  if (temperature >= 22.0) {
+    digitalWrite(BLUE, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, HIGH);
+  }
 }
