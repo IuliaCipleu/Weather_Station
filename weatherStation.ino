82b18c5bd577ac0474966e3c67a51ab4fc30da1e
Iulia Cipleu <115873337+IuliaCipleu@users.noreply.github.com>
2023-12-09 20:20:29 +0200

    Add files via upload

---
+++ weatherStation.ino
@@ -0,0 +1,248 @@
+// #include "DHT.h"
+
+// //String sendData(String command, const int timeout, boolean debug);
+
+// #define DHTPIN 7       // Digital pin connected to the DHT sensor
+// #define DHTTYPE DHT22  // Type of DHT sensor (DHT22)
+// #define BLUE 10
+// #define GREEN 11
+// #define RED 12
+// #define DEBUG true
+
+// DHT dht(DHTPIN, DHTTYPE);
+
+// void setup() {
+//   pinMode(BLUE, OUTPUT);
+//   pinMode(GREEN, OUTPUT);
+//   pinMode(RED, OUTPUT);
+//   digitalWrite(BLUE, LOW);
+//   digitalWrite(GREEN, LOW);
+//   digitalWrite(RED, LOW);
+//   Serial.begin(9600);
+//   Serial1.begin(115200);  // ESP8266 baud rate
+
+//   sendData("AT+RST\r\n", 2000, false);       // reset module
+//   // Change SSID and password
+//   sendData("AT+CWJAP=\"WeatherStation\",\"nuPl0uaAzi\"\r\n", 2000, false);
+//   sendData("AT+CWMODE=2\r\n", 1000, false);  // configure as
+//   //access point
+//   sendData("AT+CIFSR\r\n", 1000, DEBUG);   // get ip address
+//   sendData("AT+CWSAP?\r\n", 2000, DEBUG);  // get SSID info
+//   //(network name)
+//   sendData("AT+CIPMUX=1\r\n", 1000, false);  // configure for
+//   //multiple connections
+//   sendData("AT+CIPSERVER=1,80\r\n", 1000, false);  // turn on
+
+//   dht.begin();
+// }
+
+// void loop() {
+//   delay(2000);  // Wait for 2 seconds between readings
+
+//   float humidity = dht.readHumidity();
+//   float temperature = dht.readTemperature();
+
+//   // Check if any reads failed and exit early (to try again).
+//   if (isnan(humidity) || isnan(temperature)) {
+//     Serial.println("Failed to read from DHT sensor!");
+//     return;
+//   }
+
+//   Serial.print("Humidity: ");
+//   Serial.print(humidity);
+//   Serial.print("%  Temperature: ");
+//   Serial.println(temperature);
+
+//   updateLEDs(temperature);
+
+//   sendSensorData(temperature, humidity);
+// }
+
+// // void setupWiFi() {
+// //   sendData("AT+RST\r\n", 2000, false);  // Reset module
+// //   sendData("AT+CWJAP=\"WeatherStation\",\"nuPl0uaAzi\"\r\n", 2000, false);  // Change SSID and password
+// //   sendData("AT+CWMODE=2\r\n", 1000, false);  // Configure as access point
+// //   sendData("AT+CIFSR\r\n", 1000, false);  // Get IP address
+// //   sendData("AT+CWSAP?\r\n", 2000, false);  // Get SSID info (network name)
+// //   sendData("AT+CIPMUX=1\r\n", 1000, false);  // Configure for multiple connections
+// //   sendData("AT+CIPSERVER=1,80\r\n", 1000, false);  // Turn on server on port 80
+// // }
+
+// void updateLEDs(float temperature) {
+//   if (temperature < 19.0) {
+//     digitalWrite(BLUE, HIGH);
+//     digitalWrite(GREEN, LOW);
+//     digitalWrite(RED, LOW);
+//   } else if (temperature >= 19.0 && temperature < 22.0) {
+//     digitalWrite(BLUE, LOW);
+//     digitalWrite(GREEN, HIGH);
+//     digitalWrite(RED, LOW);
+//   } else {
+//     digitalWrite(BLUE, LOW);
+//     digitalWrite(GREEN, LOW);
+//     digitalWrite(RED, HIGH);
+//   }
+// }
+
+// void sendSensorData(float temperature, float humidity) {
+//   String sensorData = "<h1>Sensor Data</h1>";
+//   sensorData += "<p>Temperature: " + String(temperature) + " &deg;C</p>";
+//   sensorData += "<p>Humidity: " + String(humidity) + " %</p>";
+
+//   int connectionId = 0;  // You can modify this based on your requirements
+
+//   String cipSend = "AT+CIPSEND=";
+//   cipSend += connectionId;
+//   cipSend += ",";
+//   cipSend += sensorData.length();
+//   cipSend += "\r\n";
+
+//   sendData(cipSend, 100, DEBUG);
+//   sendData(sensorData, 150, DEBUG);
+
+//   String closeCommand = "AT+CIPCLOSE=";
+//   closeCommand += connectionId;
+//   closeCommand += "\r\n";
+//   sendData(closeCommand, 300, DEBUG);
+// }
+#include "DHT.h"
+
+#define DHTPIN 7       // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22  // Type of DHT sensor (DHT22)
+#define BLUE 10
+#define GREEN 11
+#define RED 12
+
+#define DEBUG true
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  sendData("AT+RST\r\n", 2000, false);  // reset module
+  // Change SSID and password
+  sendData("AT+CWJAP=\"WeatherStation\",\"nuPl0uaAzi\"\r\n", 2000, false);
+  sendData("AT+CWMODE=2\r\n", 1000, false);  // configure as
+  //access point
+  sendData("AT+CIFSR\r\n", 1000, DEBUG);   // get ip address
+  sendData("AT+CWSAP?\r\n", 2000, DEBUG);  // get SSID info
+  //(network name)
+  sendData("AT+CIPMUX=1\r\n", 1000, false);  // configure for
+  //multiple connections
+  sendData("AT+CIPSERVER=1,80\r\n", 1000, false);  // turn on
+  //server on port 80
+
+  pinMode(BLUE, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+  digitalWrite(BLUE, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(RED, LOW);
+  Serial.begin(9600);
+  dht.begin();
+}
+void loop() {
+  if (Serial1.available()) {
+    if (Serial1.find("+IPD,")) {
+      delay(500);
+      int connectionId = Serial1.read() - 48;  // read()
+                                               //function returns
+                                               // ASCII decimal value and 0 (the first decimal number) starts at 48
+      String webpage = "<h1>Weather Station App</h1><a href=\"/l0\"><button>ON</button></a>";
+      String cipSend = "AT+CIPSEND=";
+      cipSend += connectionId;
+      cipSend += ",";
+      webpage += "<a href=\"/l1\"><button>OFF</button></a>";
+      if (readTemperature() > 0) {
+        webpage += "<h2>Temperature:</h2>";
+        webpage += readTemperature();
+      }
+      if (readHumidity() > 0) {
+        webpage += "<h2>Humidity:</h2>";
+        webpage += readHumidity();
+      }
+      cipSend += webpage.length();
+      cipSend += "\r\n";
+      sendData(cipSend, 100, DEBUG);
+      sendData(webpage, 150, DEBUG);
+      String closeCommand = "AT+CIPCLOSE=";
+      closeCommand += connectionId;  // append connection id
+      closeCommand += "\r\n";
+      sendData(closeCommand, 300, DEBUG);
+    }
+  }
+}
+String sendData(String command, const int timeout, boolean debug) {
+  String response = "";
+  Serial1.print(command);  // send command to the esp8266
+  long int time = millis();
+  while ((time + timeout) > millis()) {
+    while (Serial1.available()) {
+      char c = Serial1.read();  // read next char
+      response += c;
+    }
+  }
+  if (response.indexOf("/l0") != -1) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  if (response.indexOf("/l1") != -1) {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  if (debug) {
+    Serial.print(response);
+  }
+  return response;
+}
+float readTemperature() {
+  //return millis();
+  //delay(2000);  // Wait for 2 seconds between readings
+
+  float temperature = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if ( isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("%  Temperature: ");
+  Serial.println(temperature);
+
+  if (temperature < 19.0) {
+    digitalWrite(BLUE, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, LOW);
+  }
+  if (temperature >= 19.0 && temperature < 22.0) {
+    digitalWrite(BLUE, LOW);
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(RED, LOW);
+  }
+  if (temperature >= 22.0) {
+    digitalWrite(BLUE, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, HIGH);
+  }
+  return temperature;
+}
+float readHumidity() {
+  //return millis();
+  //delay(2000);  // Wait for 2 seconds between readings
+
+  float humidity = dht.readHumidity();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+
+  return humidity;
+
+}
